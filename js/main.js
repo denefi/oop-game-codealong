f8c90be0cc99de21dbf7283f3b62f7e29512f810
denefi <denefi@posteo.de>
2023-03-02 15:07:40 +0100

    adds falling obstacles in intervalls with random y-axis-position.

--- js/main.js
+++ js/main.js
@@ -15,8 +15,34 @@ class Player {
         this.playerElm.style.left = this.positionX + "vw"; 
     }
 }
+class Obstacle {
+  constructor() {
+    this.positionX = Math.floor(Math.random() * 90);
+    this.positionY = 100;
+    this.obstacleDiv = document.createElement("div");
+    this.createDomElement();//create dom element
+  }
+  createDomElement() {
+    this.obstacleDiv.className = "obstacle"
+    const parentElm = document.getElementById("board");
+    this.obstacleDiv.style.left = this.positionX +"vw";
+    parentElm.appendChild(this.obstacleDiv);
+  }
+  moveDown() {
+    this.positionY--;
+    this.obstacleDiv.style.bottom = this.positionY + "vh";
 
-const myPlayer = new Player(); 
+
+  }
+}
+myPlayer = new Player();
+const obstacles = [];
+setInterval(() =>  obstacles.push(new Obstacle), 5000);
+setInterval(() => {
+  for (obstacle of obstacles) {
+    obstacle.moveDown();
+  }
+}, 100); 
 
 document.addEventListener("keydown", (event) =>{
     if (event.key === "ArrowLeft") {
