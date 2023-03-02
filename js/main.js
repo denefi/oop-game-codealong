ceae3e9cb7e14351b34419536e374876e5c7ea0e
denefi <denefi@posteo.de>
2023-03-02 15:58:57 +0100

    Add documentation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Define Class Player
 class Player {
     constructor() {
         this.positionX = 0;
@@ -15,15 +16,19 @@ class Player {
         this.playerElm.style.left = this.positionX + "vw"; 
     }
 }
+//Define class Obstacle
 class Obstacle {
   constructor() {
+    //generate random positon from which the obstacle will fall.
+    //Max number is 90 because the obstacle has a width of 10vw.
     this.positionX = Math.floor(Math.random() * 90);
     this.positionY = 100;
     this.obstacleDiv = document.createElement("div");
     this.parentElm = document.getElementById("board");
-    this.createDomElement();//create dom element
+    //call the addDomElement method to append the new obstacle div to the DOM. 
+    this.addDomElement();
   }
-  createDomElement() {
+  addDomElement() {
     this.obstacleDiv.className = "obstacle"
     this.obstacleDiv.style.left = this.positionX +"vw";
     this.parentElm.appendChild(this.obstacleDiv);
@@ -34,24 +39,28 @@ class Obstacle {
     this.checkDeath();
   }
   checkDeath() {
+    //remove Div from DOM and the obstacle from obstacles array. 
     if(this.positionY < -10) {
       this.parentElm.removeChild(this.obstacleDiv);
       obstacles.shift();
     }
   }
 }
+//initialize Player 
 myPlayer = new Player();
 const obstacles = [];
+//set Intervall for creatign objects
 setInterval(() => { 
   obstacles.push(new Obstacle);
   console.log(obstacles)
 }, 5000);
+//set interval for movement of obstacles
 setInterval(() => {
   for (obstacle of obstacles) {
     obstacle.moveDown();
   }
 }, 100); 
-
+//Event listener for Player movement with arrow keys. 
 document.addEventListener("keydown", (event) =>{
     if (event.key === "ArrowLeft") {
         myPlayer.moveLeft();
